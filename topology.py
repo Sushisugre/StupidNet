f6f40804c2c3862c5191ce7230a08c0612093a2f
Sushi <just.s444@gmail.com>
2016-05-03 08:23:48 -0400

    setup topo

--- topology.py
+++ topology.py
@@ -17,6 +17,27 @@ class MixTopo(Topo):
         self.addLink( h2, s1 )
         self.addLink( h3, s1 )
         self.addLink( s1, s2 )
-        self.addLink( s2, h4 )        
+        self.addLink( s2, h4 ) 
+  
 
-topos = { 'mixtopo': ( lambda: MixTopo() ) }
+def setup():
+    topo = MixTopo()
+    #net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, controller=POXcontroller1)
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, controller=partial(RemoteController, ip='127.0.0.1', port=6633))
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    h1,h2,h3=net.get('h1','h2','h3')
+    h4,h5,h6=net.get('h4','h5','h6')
+    h1.setMAC("0:0:0:0:1:0")
+    h2.setMAC("0:0:0:0:2:0")
+    h3.setMAC("0:0:0:0:3:0")
+    h4.setMAC("0:0:0:0:4:0")
+    h5.setMAC("0:0:0:0:5:0")
+    h6.setMAC("0:0:0:0:6:0")
+    CLI(net)
+    net.stop()
+ 
+if __name__ == '__main__':
+    setLogLevel('info')
+    setup()
